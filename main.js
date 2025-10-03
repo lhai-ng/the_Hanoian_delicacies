41322d6ca3a9b47f716694b70a53e7b637ea9bee
Long Hai Nguyen <longhai14149@gmail.com>
2025-10-03 21:50:23 +0700

    Completed Hero and Header animation

--- main.js
+++ main.js
@@ -14,12 +14,63 @@ gsap.ticker.lagSmoothing(0);
 
 const preloader = document.querySelector("#preloader");
 const anchorsPreloader = document.querySelectorAll("#preloader h6");
+const header = document.querySelector("header");
 const display = document.querySelectorAll(".display");
 const texts = document.querySelectorAll(".text");
+const heroImages = document.querySelectorAll("#hero img");
+const hero = document.querySelector("#hero");
+
+const audioSwitch = document.querySelector(".audio-switch");
+const soundOn = document.querySelector(".sound-on");
+const soundOff = document.querySelector(".sound-off");
+const bars = document.querySelectorAll(".wave-animation .bar");
+const audio = document.querySelector("audio");
+let isPlaying = true;
+
+function playAudio(isPlaying) {
+  if (isPlaying) {
+    soundOn.style.display = "block";
+    soundOff.style.display = "none";
+    bars.forEach((bar, index) => {
+      bar.anim = gsap.fromTo(bar, {
+        scaleY: .8,
+      }, {
+        scaleY: 1.1,
+        duration: .7,
+        yoyo: true,
+        repeat: -1,
+        ease: 'sine.out',
+        delay: index * .15,
+      });
+    })
+    audio.volume = .4;
+    audio.play();
+  } else {
+    soundOn.style.display = "none";
+    soundOff.style.display = "block";
+    bars.forEach((bar) => {
+      if (bar.anim) {
+        bar.anim.kill();
+      }
+      bar.anim = gsap.to(bar, {
+        scaleY: 1,
+        duration: .3,
+        ease: "sine.out",
+      })
+    })
+    audio.pause()
+  }
+}
+
+audioSwitch.onclick = () => {
+  playAudio(isPlaying);
+  isPlaying = !isPlaying;
+}
+
 
 const tl1 = gsap.timeline();
 anchorsPreloader.forEach((anchor, index) => {
-  const delay = index === 0 ? .5 : 0
+  const delay = index === 0 ? .5 : 0;
   tl1.set(document.documentElement, {
     paddingRight: "16px",
     overflow: "hidden",
@@ -34,9 +85,9 @@ anchorsPreloader.forEach((anchor, index) => {
   if (index !== (anchorsPreloader.length - 1)) {
     tl1.to(anchor, {
     opacity: 0,
-    delay: .4,
+    delay: .2,
     duration: .4,
-    ease: "power4.out"
+    ease: "power4.out",
   });
   }
 })
@@ -45,8 +96,17 @@ tl1.to(preloader, {
   yPercent: -105,
   duration: 1.8,
   borderRadius: "50%",
-  ease: "expo.inOut"
+  ease: "expo.inOut",
+  onStart: () => {
+    playAudio(false);
+  }
 },"<");
+
+tl1.from(header, {
+  yPercent: -100,
+  ease: "sine.out",
+  duration: 2.5,
+}, "<")
 
 tl1.set(document.documentElement, {
   paddingRight: "0",
@@ -83,9 +143,32 @@ function setupSplits(
   })
 }
 
-setupSplits(display, 2.5, "sine.out", 100, .1);
-setupSplits(texts, 3, "sine.out", 100, .1);
+setupSplits(display, 1.75, "sine.out", 100, .1);
+setupSplits(texts, 2, "sine.out", 100, .1);
 
+heroImages.forEach((img, index) => {
+  const imageWrapper = Object.assign(document.createElement("div"), { className: `image-wrapper-${index+1}`});
+  img.replaceWith(imageWrapper);
+  imageWrapper.appendChild(img);
+
+  img.anim = tl1.from(img, {
+    duration: 2.5,
+    ease: "power2.out",
+    xPercent: -100,
+    delay: 0,
+  }, "<");
+});
+
+tl1.to(hero, {
+  scrollTrigger: {
+    trigger: hero,
+    start: 'top 0%',
+    end: 'bottom 5%',
+    scrub: true,
+  },
+  top: "-640px",
+  ease: "power2.out"
+}, "<");
 
 
 
